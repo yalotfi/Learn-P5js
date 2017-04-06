8757c1084323cfe745337636efb8eaf1b653220e
Yaseen Lotfi <yalotfi@outlook.com>
2017-04-05 23:52:59 -0400

    methods to rise and fall instead of simple update

--- Intro-to-P5/Box.js
+++ Intro-to-P5/Box.js
@@ -1,22 +1,36 @@
 // Box Constructor (inherit position and dimension from Bubble)
 function Box(xPos, yPos, radius) {
   Bubble.call(this, xPos, yPos, radius)
+
   this.display = function() {
     rectMode(CENTER);
     fill(255);
     rect(this.xPos, this.yPos, this.radius * 2, this.radius * 2);
   }
 
-  this.checkEdge = function() {
+  this.checkBottom = function() {
     return (this.yPos >= height - this.radius) ? true : false;
   }
 
-  this.update = function() {
+  this.checkTop = function() {
+    return (this.yPos <= this.radius) ? true : false;
+  }
+
+  this.fall = function() {
     this.xPos = this.xPos;
-    if (this.checkEdge()) {
+    if (this.checkBottom()) {
       this.yPos = height - this.radius;
     } else {
       this.yPos += 2;
+    }
+  }
+
+  this.rise = function() {
+    this.xPos = this.xPos;
+    if (this.checkTop()) {
+      this.yPos = this.radius;
+    } else {
+      this.yPos -= 2;
     }
   }
 }
